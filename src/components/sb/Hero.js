3288f9177516577336687c595cef4e7b7ec02a60
ErikBergloef <47384098+ErikBergloef@users.noreply.github.com>
2025-09-03 16:18:28 +0200

    hej

--- src/components/sb/Hero.js
+++ src/components/sb/Hero.js
@@ -1,20 +1,11 @@
 import { storyblokEditable } from "@storyblok/react";
+import Image from "next/image";
 
 export default function Hero({ blok }) {
   return (
     <div
       {...storyblokEditable(blok)}
-      className="relative bg-cover bg-no-repeat bg-center h-[50vh] flex items-center justify-center text-white"
-      style={{
-        backgroundImage: `url(${blok?.image?.filename})`,
-      }}
-    >
-        <div className="flex flex-col items-center justify-center">
-
-       
-      <h1 className="text-6xl font-bold">{blok.title}</h1>
-      <p className="text-2xl">{blok.paragraph}</p>
-       </div>
+      className="bg-white p-8"> 
     </div>
   );
 }
