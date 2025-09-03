8fcc6a45c22e03f709c71b00f170d4d1efaec723
Felix0291 <felix.tapper@gmail.com>
2025-09-03 17:23:59 +0200

    img, title, price reders on /products page fix

--- src/components/products/ProductCard.js
+++ src/components/products/ProductCard.js
@@ -1,4 +1,3 @@
-import { storyblokEditable } from "@storyblok/react";
 import Link from "next/link";
 import Image from "next/image";
 
@@ -6,9 +5,9 @@ export default function ProductCard({ title, price, image, slug }) {
   return (
     <Link
       href={`/products/${slug}`}
-      className="rounded-sm p-4 hover:shadow-lg transition-shadow duration-300"
+      className="rounded-lg p-4 hover:shadow-lg bg-white transition-shadow duration-300 block"
     >
-      <div className="w-full h-48 relative mb-4">
+      <div className="w-full h-48 relative mb-4 rounded-lg overflow-hidden bg-gray-200">
         {image ? (
           <Image
             src={image}
@@ -23,6 +22,13 @@ export default function ProductCard({ title, price, image, slug }) {
             No Image{" "}
           </div>
         )}
+      </div>
+
+      <div className="flex justify-between items-center mt-2">
+        <h2 className="text-sm">{title}</h2>
+        <p className="text-sm">
+          {price ? `$${price}` : "N/A"}
+        </p>
       </div>
     </Link>
   );
