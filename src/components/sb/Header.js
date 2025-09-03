078138fca63a80374832d1351f99ea4bc33e4886
ErikBergloef <47384098+ErikBergloef@users.noreply.github.com>
2025-09-03 13:04:53 +0200

    added header style

--- src/components/sb/Header.js
+++ src/components/sb/Header.js
@@ -1,24 +1,54 @@
 import { storyblokEditable } from "@storyblok/react";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Header({ blok }) {
   console.log("header::::", blok);
   return (
     <div
       {...storyblokEditable(blok)}
-      className="w-full bg-fixed h-[100px]
-      flex items-center justify-between px-8 bg-white shadow-md text-black"
+      className="w-full h-[100px] flex items-center justify-between px-8 bg-white shadow-md text-black border-b"
     >
-   
-      <nav>
-        <ul className="flex space-x-6">
-          {blok.nav_items?.map((navItem) => (
-            <li key={navItem._uid}>
-              <Link href={navItem.link.story?.url || "/"}>{navItem.label}</Link>
-            </li>
-          ))}
-        </ul>
-      </nav>
+
+      {/* Vänster sektion för logotyp och nav_items */}
+      <div className="flex items-center space-x-8">
+
+        {/* Logo */}
+        <div className="flex-none">
+          <Image src={blok.logo.filename} alt="Logo" width={70} height={40} />
+        </div>
+
+        {/* Navigation */}
+        <nav>
+          <ul className="flex space-x-8 font-bold">
+            {blok.nav_items?.map((navItem) => (
+              <li key={navItem._uid}>
+                <Link href={navItem.link.story?.url || "/"}>
+                  {navItem.label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </div>
+
+      {/* Höger sektion för ikoner */}
+      <div className="flex-1 flex justify-end items-center">
+
+        {/* Här lägger vi till ikonerna */}
+        <div className="flex gap-4">
+          <Link href="/search">
+
+            <Image src="/Search_logo.png" alt="Search" width={30} height={30} />
+          </Link>
+          <Link href="/account">
+            <Image src="/Account_logo.png" alt="Search" width={30} height={30} />
+          </Link>
+          <Link href="/cart">
+            <Image src="/Cart_logo.png" alt="Search" width={30} height={30} />
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
