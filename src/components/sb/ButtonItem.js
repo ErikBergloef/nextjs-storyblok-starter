fc42ec62ca76f97a1b2c01e1633b4e6ed2f44c74
Felix0291 <felix.tapper@gmail.com>
2025-09-08 16:22:14 +0200

    color option button

--- src/components/sb/ButtonItem.js
+++ src/components/sb/ButtonItem.js
@@ -1,9 +1,27 @@
-export default function ButtonItem({ blok }) {
-    console.log("Button data from Storyblok:", blok);
+// src/components/sb/ButtonItem.js
+import { storyblokEditable } from "@storyblok/react";
 
-    return(
-        <button className="px-4 border text-black rounded-sm flex space-ibetween 2px">
-            {blok?.label}
-        </button>
+export default function ButtonItem({ blok }) {
+  if (blok.color_image && blok.color_image.filename) {
+    return (
+      <button 
+        {...storyblokEditable(blok)} 
+        className="w-8 h-8 rounded-full border border-black  overflow-hidden"
+      >
+        <img 
+          src={blok.color_image.filename} 
+          alt={blok.color_image.alt || "Color swatch"} 
+          className="w-full h-full object-cover" 
+        />
+      </button>
     );
+  }
+  return (
+    <button 
+      {...storyblokEditable(blok)} 
+      className="px-4 py-2 border border-black text-sm font-medium hover:bg-gray-100 transition-colors"
+    >
+      {blok?.label}
+    </button>
+  );
 }
