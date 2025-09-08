799ff3c7bdaa215fcc121a65596ba064a4ec226a
Felix0291 <felix.tapper@gmail.com>
2025-09-08 13:41:20 +0200

    sizes

--- src/app/products/[slug]/page.js
+++ src/app/products/[slug]/page.js
@@ -25,6 +25,9 @@ export default async function ProductPage({ params }) {
             />
           </div>
         )}
+        <div className="flex space-x-0.5">
+          {product.sizes}
+        </div>
   
         <div className="flex flex-col justify-start space-y-4">
           <h1 className="text-lg font-bold">{product.title}</h1>
